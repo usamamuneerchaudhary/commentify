bd62093557d646ab61200758ac8ffcc05d43e9f8
Usama Muneer <usama@rentable.pk>
2023-04-02 18:58:22 +0100

    coment partials

---
+++ resources/views/livewire/partials/comment-form.blade.php
@@ -0,0 +1,27 @@
+<form class="mb-6" wire:submit.prevent="{{$method}}">
+    <div
+        class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700
+             ">
+        <label for="{{$inputId}}" class="sr-only">{{$inputLabel}}</label>
+        <textarea id="{{$inputId}}" rows="6"
+                  class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none
+                              dark:text-white dark:placeholder-gray-400 dark:bg-gray-800 @error($state.'.body')
+                              border-red-500 @enderror"
+                  placeholder="Write a comment..."
+                  wire:model.defer="{{$state}}.body"
+                  oninput="detectAtSymbol()"
+        ></textarea>
+        @if(!empty($users) && $users->count() > 0)
+            @include('commentify::livewire.partials.dropdowns.users')
+        @endif
+        @error($state.'.body')
+        <p class="mt-2 text-sm text-red-600">
+            {{$message}}
+        </p>
+        @enderror
+    </div>
+    <button type="submit"
+            class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
+        {{$button}}
+    </button>
+</form>
---
+++ resources/views/livewire/partials/dropdowns/users.blade.php
@@ -0,0 +1,15 @@
+<div class="z-10 bg-white rounded-lg shadow w-60 dark:bg-gray-700">
+    <ul class="h-48 py-2 overflow-y-auto text-gray-700 dark:text-gray-200"
+    >
+        @foreach($users as $user)
+            <li wire:click="selectUser('{{ $user->name }}')">
+                <a class="flex items-center px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                    <img class="w-6 h-6 mr-2 rounded-full" src="{{$user->avatar()}}"
+                         alt="{{ $user->name }}">
+                    {{ $user->name }}
+                </a>
+            </li>
+        @endforeach
+
+    </ul>
+</div>
