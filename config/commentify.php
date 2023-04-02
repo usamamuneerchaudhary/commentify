e98cb9fd21fc04855a43edc22ef8566d97970bf0
Usama Muneer <usama@rentable.pk>
2023-04-02 05:24:33 +0100

    `commentify` config file

---
+++ config/commentify.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'users_route_prefix' => 'users', //set this prefix to anything that you wish to use for users profile routes
+
+    'css_framework' => 'tailwind', // or 'bootstrap' //TODO
+];
