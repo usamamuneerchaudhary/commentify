ed81382a8e105069c77c8761587e2a3248a21dbb
Usama Muneer <usama@rentable.pk>
2023-04-03 11:08:50 +0100

    add `User` model from `Auth\User`

---
+++ src/Models/User.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Usamamuneerchaudhary\Commentify\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as BaseUser;
+use Usamamuneerchaudhary\Commentify\Database\Factories\UserFactory;
+use Usamamuneerchaudhary\Commentify\Traits\HasUserAvatar;
+
+class User extends BaseUser
+{
+    use HasUserAvatar, HasFactory;
+
+    /**
+     * @var string
+     */
+    protected $table = 'users';
+
+    protected static function newFactory()
+    {
+        return UserFactory::new();
+    }
+}
