d0ec1eaa929154f09239946c0c03ccdbcb0c06bd
Usama Muneer <usama@rentable.pk>
2023-04-05 02:53:34 +0100

    refactoring

--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -7,26 +7,13 @@
                     ({{$comments->count()}})</h2>
             </div>
             @auth
-                <form class="mb-6" wire:submit.prevent="postComment">
-                    <div
-                        class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-                        <label for="comment" class="sr-only">Your comment</label>
-                        <textarea id="comment" rows="6"
-                                  class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
-                                  placeholder="Write a comment..." required
-                                  wire:model.defer="newCommentState.body"
-                        ></textarea>
-                        @error('newCommentState.body')
-                        <p class="mt-2 text-sm text-red-600">
-                            {{$message}}
-                        </p>
-                        @enderror
-                    </div>
-                    <button type="submit"
-                            class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
-                        Post comment
-                    </button>
-                </form>
+                @include('commentify::livewire.partials.comment-form',[
+                    'method'=>'postComment',
+                    'state'=>'newCommentState',
+                    'inputId'=> 'comment',
+                    'inputLabel'=> 'Your comment',
+                    'button'=>'Post comment'
+                ])
             @else
                 <a class="mt-2 text-sm" href="/login">Log in to comment!</a>
             @endauth
--- resources/views/livewire/partials/comment-form.blade.php
+++ resources/views/livewire/partials/comment-form.blade.php
@@ -1,4 +1,5 @@
 <form class="mb-6" wire:submit.prevent="{{$method}}">
+    @csrf
     <div
         class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700
              ">
