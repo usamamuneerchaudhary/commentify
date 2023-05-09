c5db60c162e82b4e50500645cd21d0b9f27b789d
Usama Muneer <usama@rentable.pk>
2023-05-09 01:24:02 +0100

    add key to selectUser list to keep track of the element

--- resources/views/livewire/partials/dropdowns/users.blade.php
+++ resources/views/livewire/partials/dropdowns/users.blade.php
@@ -2,7 +2,7 @@
     <ul class="h-48 py-2 overflow-y-auto text-gray-700 dark:text-gray-200"
     >
         @foreach($users as $user)
-            <li wire:click="selectUser('{{ $user->name }}')">
+            <li wire:click="selectUser('{{ $user->name }}')" wire:key="{{ $user->id }}">
                 <a class="flex items-center px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                     <img class="w-6 h-6 mr-2 rounded-full" src="{{$user->avatar()}}"
                          alt="{{ $user->name }}">
