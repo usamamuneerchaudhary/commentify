55a01e11c8461c13ddd26c0c9cdd655672157334
Usama Munir <hello@usamamuneer.me>
2024-02-27 04:55:01 +0000

    fix for user mention not working
    closes: #13

--- resources/views/livewire/comment.blade.php
+++ resources/views/livewire/comment.blade.php
@@ -14,9 +14,9 @@
 
                 <div class="flex items-center">
                     <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white"><img
-                            class="mr-2 w-6 h-6 rounded-full"
-                            src="{{$comment->user->avatar()}}"
-                            alt="{{$comment->user->name}}">{{Str::ucfirst($comment->user->name)}}</p>
+                                class="mr-2 w-6 h-6 rounded-full"
+                                src="{{$comment->user->avatar()}}"
+                                alt="{{$comment->user->name}}">{{Str::ucfirst($comment->user->name)}}</p>
                     <p class="text-sm text-gray-600 dark:text-gray-400">
                         <time pubdate datetime="{{$comment->presenter()->relativeCreatedAt()}}"
                               title="{{$comment->presenter()->relativeCreatedAt()}}">
@@ -31,14 +31,14 @@
                         <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
                              xmlns="http://www.w3.org/2000/svg">
                             <path
-                                d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z">
+                                    d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z">
                             </path>
                         </svg>
                     </button>
                     <!-- Dropdown menu -->
                     @if($showOptions)
                         <div
-                            class="absolute z-10 top-full right-0 mt-1 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
+                                class="absolute z-10 top-full right-0 mt-1 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
                             <ul class="py-1 text-sm text-gray-700 dark:text-gray-200">
                                 @can('update',$comment)
                                     <li>
@@ -53,15 +53,15 @@
                                     <li>
 
                                         <button
-                                            x-on:click="confirmCommentDeletion"
-                                            x-data="{
+                                                x-on:click="confirmCommentDeletion"
+                                                x-data="{
                                     confirmCommentDeletion(){
                                         if(window.confirm('You sure to delete this comment?')){
                                             @this.call('deleteComment')
                                         }
                                     }
                                 }"
-                                            class="block w-full text-left py-2 px-4 hover:bg-gray-100
+                                                class="block w-full text-left py-2 px-4 hover:bg-gray-100
                                             dark:hover:bg-gray-600 dark:hover:text-white">
                                             Delete
                                         </button>
@@ -145,9 +145,8 @@
 
             if (atSymbolPosition !== -1) {
                 const searchTerm = textBeforeCursor.substring(atSymbolPosition + 1);
-
                 if (searchTerm.trim().length > 0) {
-                    window.livewire.dispatch('getUsers', searchTerm);
+                    @this.dispatchSelf('getUsers', { searchTerm: searchTerm});
                 }
             }
         }
