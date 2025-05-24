47fc80ffb0035c67e06bcc700243955ff6dfa884
Usama Munir <hello@usamamuneer.me>
2025-05-24 19:03:47 +0100

    added option for readonly mode

--- config/commentify.php
+++ config/commentify.php
@@ -5,5 +5,5 @@ return [
     'pagination_count' => 10,
     'css_framework' => 'tailwind', // or 'bootstrap'
     'comment_nesting' => true, // set to false if you don't want to allow nesting of comments
-
+    'read_only' => false, // set to true if you want to make comments read only
 ];
