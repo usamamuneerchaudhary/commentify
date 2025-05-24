2a0daf25ca0b4cfacca42242940adaa0e421345f
Usama Munir <hello@usamamuneer.me>
2025-05-24 19:12:42 +0100

    added lang support

---
+++ lang/en/commentify.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'comments' => [
+        'discussion' => 'Discussion',
+        'your_comment' => 'Your comment',
+        'post_comment' => 'Post comment',
+        'login_to_comment' => 'Log in to comment!',
+        'no_comments' => 'No comments yet!',
+        'edit' => 'Edit',
+        'delete' => 'Delete',
+        'delete_confirm' => 'You sure to delete this comment?',
+        'reply' => 'Reply',
+        'view_replies' => 'View all Replies (:count)',
+        'hide_replies' => 'Hide Replies',
+        'write_comment' => 'Write a comment...',
+        'success' => 'Success!',
+        'likes' => 'likes',
+        'your_reply' => 'Your Reply',
+        'edit_comment' => 'Edit Comment',
+        'post_reply' => 'Post Reply',
+        'read_only_message' => 'Commenting is currently disabled.',
+        'banned_message' => 'You are temporarily restricted from commenting.',
+    ],
+];
