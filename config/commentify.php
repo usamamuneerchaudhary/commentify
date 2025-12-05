39b7d880a57e85816acb763fb89c23e510f51326
Usama Munir <hello@usamamuneer.me>
2025-12-05 18:19:04 +0000

    updated config

--- config/commentify.php
+++ config/commentify.php
@@ -3,7 +3,7 @@
 return [
     'users_route_prefix' => 'users', //set this prefix to anything that you wish to use for users profile routes
     'pagination_count' => 10,
-    'css_framework' => 'tailwind', // or 'bootstrap'
+    'css_framework' => 'tailwind', // Options: 'tailwind' or 'bootstrap'
     'comment_nesting' => true, // set to false if you don't want to allow nesting of comments
     'read_only' => false, // set to true if you want to make comments read only
     'default_sort' => 'newest', // newest, oldest, most_liked, most_replied
