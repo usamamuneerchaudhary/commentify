4923a262a758ddda9060facb292b0e0dc7b65253
Usama Munir <hello@usamamuneer.me>
2025-12-06 14:34:10 +0000

    check if filament plugin is installed or not

--- src/Filament/CommentifyPlugin.php
+++ src/Filament/CommentifyPlugin.php
@@ -17,6 +17,12 @@ class CommentifyPlugin implements Plugin
 
     public function register(Panel $panel): void
     {
+        // Check if Filament is installed
+        if (!class_exists(\Filament\Filament::class)) {
+            throw new \RuntimeException(
+                'Filament is not installed. Please install it first: composer require filament/filament'
+            );
+        }
         $panel
             ->resources([
                 CommentResource::class,
