79919bad12bfceffe7296a1e141020157314ce48
Usama Munir <hello@usamamuneer.me>
2025-12-25 17:28:44 +0000

    added migration for `is_approved` column in comments table

---
+++ database/migrations/2025_12_25_170921_add_is_approved_to_comments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->boolean('is_approved')->default(false)->after('body');
+            $table->index('is_approved');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dropIndex(['is_approved']);
+            $table->dropColumn('is_approved');
+        });
+    }
+};
