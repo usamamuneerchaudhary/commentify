118d17a11ce02fdf415e8af77b22bd0f58bf515d
Usama Munir <hello@usamamuneer.me>
2026-09-09 00:44:47 +0100

    Guest policy
    Allow guests to create comments when `allow_guests` is on.
    
    Guests still cannot edit or delete.

--- src/Policies/CommentPolicy.php
+++ src/Policies/CommentPolicy.php
@@ -4,20 +4,26 @@ namespace Usamamuneerchaudhary\Commentify\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
 use Illuminate\Auth\Access\Response;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Usamamuneerchaudhary\Commentify\Models\Comment;
 
 class CommentPolicy
 {
     use HandlesAuthorization;
 
-
     /**
      * Determine whether the user can view any comments.
-     * @param $user
-     * @return Response
      */
-    public function create($user): Response
+    public function create(?Authenticatable $user = null): Response
     {
+        if ($user === null && config('commentify.allow_guests', false)) {
+            return Response::allow();
+        }
+
+        if ($user === null) {
+            return Response::deny(__('commentify::commentify.comments.login_to_comment'), 401);
+        }
+
         // Check if the user is temporarily banned from commenting
         if (method_exists($user, 'isCommentBanned') && $user->isCommentBanned()) {
             return Response::deny(__('commentify::commentify.comments.banned_message'), 403);
@@ -27,31 +33,34 @@ class CommentPolicy
     }
 
     /**
-     * @param $user
-     * @param  Comment  $comment
-     * @return Response
+     * @param  Authenticatable  $user
      */
     public function update($user, Comment $comment): Response
     {
+        if ($comment->isGuest()) {
+            return Response::denyWithStatus(401);
+        }
         if (method_exists($user, 'isCommentBanned') && $user->isCommentBanned()) {
             return Response::deny(__('commentify::commentify.comments.banned_message'), 403);
         }
+
         return $user->id === $comment->user_id
             ? Response::allow()
             : Response::denyWithStatus(401);
     }
 
-
     /**
-     * @param $user
-     * @param  Comment  $comment
-     * @return Response
+     * @param  Authenticatable  $user
      */
     public function destroy($user, Comment $comment): Response
     {
+        if ($comment->isGuest()) {
+            return Response::denyWithStatus(401);
+        }
         if (method_exists($user, 'isCommentBanned') && $user->isCommentBanned()) {
             return Response::deny(__('commentify::commentify.comments.banned_message'), 403);
         }
+
         return $user->id === $comment->user_id
             ? Response::allow()
             : Response::denyWithStatus(401);
